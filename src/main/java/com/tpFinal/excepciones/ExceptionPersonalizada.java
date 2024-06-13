da2bddb9813ebb7f658ac5383d40bda75b54c66c
gastyft <gastyftacla1@hotmail.com>
2024-06-13 11:03:31 -0300

    Comentario en ExceptionPersonalizada

--- src/main/java/com/tpFinal/excepciones/ExceptionPersonalizada.java
+++ src/main/java/com/tpFinal/excepciones/ExceptionPersonalizada.java
@@ -4,4 +4,8 @@ public class ExceptionPersonalizada extends Exception{
     public ExceptionPersonalizada(String message) {
         super(message);
     }
+
+    //TODO VER SI FUNCIONA LLAMANDO A JOPTIONMESSAGE DE SWING. Deberia funcionar porque es independiente
+    //TODO a lo que ocurra. ENtonces si lanza excepcion lanzaria es cartelito de Swing con el mensaje
+    //TODO deseado personalizado
 }
