faa76d985a821a0a7f4b0e00421dd7ed5f9adf46
maurisebastian <21maurimartinez@gmail.com>
2024-06-19 19:50:03 -0300

    profesor listar cursos

--- src/main/java/com/tpFinal/swing/MenuProfesor.java
+++ src/main/java/com/tpFinal/swing/MenuProfesor.java
@@ -1,9 +1,14 @@
 package com.tpFinal.swing;
 
+import com.tpFinal.entidades.Alumno;
 import com.tpFinal.entidades.Curso;
 import com.tpFinal.entidades.Profesor;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class MenuProfesor extends JDialog {
@@ -19,6 +24,10 @@ public class MenuProfesor extends JDialog {
     private JLabel legajo;
     private JList<Curso> cursoslist;
     private JButton buscarButton;
+    private JList<Alumno> alumnoslist1;
+    private JLabel nombrecurso;
+    private JLabel inicioCurso;
+    private JLabel finCurso;
 
     public MenuProfesor(Profesor profesor, List<Curso> cursosDisponibles) {
 
@@ -37,6 +46,51 @@ public class MenuProfesor extends JDialog {
         logOutButton.addActionListener(e ->{
             {
                 setVisible(false);
+            }
+        });
+
+    }
+
+    private void inicializarCursos(){
+
+        DefaultListModel<Curso> listModel = new DefaultListModel<>();
+
+        for (Curso curso : profesor.getCursos()) {
+            listModel.addElement(curso);
+        }
+        cursoslist.setModel(listModel);
+        configurarRendererJListCursos();
+
+        buscarButton.addActionListener(e -> {
+
+            Curso cursoseleccionado= cursoslist.getSelectedValue();
+            nombrecurso.setText(cursoseleccionado.getCursosNombre().name());
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+            String formattedDateTimeinicio = cursoseleccionado.getFecha().getFechaInicio().format(formatter);
+            String formattedDateTimefin = cursoseleccionado.getFecha().getFechaFin().format(formatter);
+            inicioCurso.setText(formattedDateTimeinicio);
+            finCurso.setText(formattedDateTimefin);
+
+
+        });
+
+
+
+    }
+    private void configurarRendererJListCursos() {
+        cursoslist.setCellRenderer(new ListCellRenderer<Curso>() {
+            @Override
+            public Component getListCellRendererComponent(JList<? extends Curso> list, Curso value, int index, boolean isSelected, boolean cellHasFocus) {
+                JLabel label = new JLabel(value.getCursosNombre().name());
+                if (isSelected) {
+                    label.setBackground(list.getSelectionBackground());
+                    label.setForeground(list.getSelectionForeground());
+                } else {
+                    label.setBackground(list.getBackground());
+                    label.setForeground(list.getForeground());
+                }
+                label.setOpaque(true);
+                return label;
             }
         });
     }
